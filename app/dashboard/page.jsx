33cbeeadd2160f6cdfeb824d7ef96cb80686ffa3
VATTIKUTI RAGHAVENDRA <vattikutiraghavendra3@gmail.com>
2023-12-28 02:32:24 +0530

    userspage

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,31 @@
-const Dashboard = () => {
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
-}
+// import { cards } from "../lib/data";
+import Card from "../ui/dashboard/card/card";
+import RightBar from "../ui/dashboard/rightbar/rightbar"
+import Chart from "../ui/dashboard/chart/chart";
+import styles from "../ui/dashboard/dashboard.module.css";
 
-export default Dashboard
+import Transactions from "../ui/dashboard/transactions/transactions";
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          {/* {cards.map((item) => (
+            <Card item={item} key={item.id} />
+          ))} */}
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,64 @@
+import styles from "../../ui/dashboard/users/user.module.css"
+import Search from '../../ui/dashboard/search/search'
+import Link from "next/link"
 const UsersPage = () => {
     return (
-        <div>
-            UsersPage
+        <div className={styles.container}>
+            <div className={styles.top}>
+                <Search placeholder="Search here"/>
+                <Link href="/dashboard/users/add">
+                    <button className={styles.addButton}>Add New</button>
+                </Link>
+            </div>
+            <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created At</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt?.toString().slice(4, 16)}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <form action={deleteUser}>
+                    <input type="hidden" name="id" value={(user.id)} />
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </form>
+                </div>
+              </td>
+            </tr>
+          ))} */}
+        </tbody>
+      </table>
         
         </div>
     )
